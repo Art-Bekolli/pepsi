86b1a7741e692233e2296643586113c1c64ebd9a
Art-Bekolli <abekolli57@gmail.com>
2023-07-07 18:45:22 +0200

    random post gen

--- resources/functions.php
+++ resources/functions.php
@@ -120,7 +120,7 @@ function wporg_options_page_html() {
 	?>
 	<div class="wrap">
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-		<form action="options.php" method="post">
+		<form class="random-form" action="options.php" method="post">
 			<?php
 			// output security fields for the registered setting "wporg"
 			settings_fields( 'wporg' );
